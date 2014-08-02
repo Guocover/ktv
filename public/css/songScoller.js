8c3e87be73f6219a53babb453438f9ab3e72ba63
cover <446227103@qq.com>
2014-08-02 17:21:31 +0800

    整合

--- public/css/songScoller.js
+++ public/css/songScoller.js
@@ -1,12 +1,13 @@
 $(".recentSongWrapper").click(function(){
 	var current = $(".recentSongWrapper .currentSong").attr("value"); 
 	var songForm = $(".songForm");
-
+	var transition_left = {"margin-left":"-140px","top":"90px","width":"70px","height":"70px"};	
+	var transition_center = {"margin-left":"-50px","top":"40px","width":"100px","height":"100px"};	
+	var transition_right = {"margin-left":"80px","top":"90px","width":"100px","height":"100px"};	
+	var transition_behind = {"margin-left":"-35px","top":"90px","width":"70px","height":"70px"};	
 	var len = songForm.length;	
-	for(var i = 0;i < len ;i++)
-	{
-		
-	}
+	
+	songForm.eq(0).css(transition_behind);
 
 
 
