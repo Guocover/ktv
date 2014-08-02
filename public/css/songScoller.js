ee91dd3def879cc08b8e6c5944223f5f3ae5b258
cover <446227103@qq.com>
2014-08-02 15:53:47 +0800

    登录，和歌单

---
+++ public/css/songScoller.js
@@ -0,0 +1,15 @@
+$(".recentSongWrapper").click(function(){
+	var current = $(".recentSongWrapper .currentSong").attr("value"); 
+	var songForm = $(".songForm");
+
+	var len = songForm.length;	
+	for(var i = 0;i < len ;i++)
+	{
+		
+	}
+
+
+
+}){
+
+}
